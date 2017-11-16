f061967e042bb062c88a8094a5a54acbd8732cfc
Joe Martella <martellaj@live.com>
2017-11-16 08:18:03 -0800

    Adds list insertion functions

--- dataStructures/linkedList/linkedList.js
+++ dataStructures/linkedList/linkedList.js
@@ -21,7 +21,7 @@ List.prototype.addFirst = function(data) {
     if (!this.tail) {
         this.tail = this.head;
     }
-};
+}
 
 /**
  * Creates a ListNode with the given data value
@@ -61,6 +61,66 @@ List.prototype.find = function(data) {
     return null;
 }
 
+/**
+ * Creates a new ListNode with the data value, and
+ * inserts it after the node with the target value.
+ * If the target doesn't exist, the new node is not
+ * inserted in the list.
+ * @param {*} data
+ * @param {*} target
+ */
+List.prototype.insertAfter = function(data, target) {
+    const targetNode = this.find(target);
+
+    if (targetNode) {
+        const newNode = new ListNode(data);
+        newNode.next = targetNode.next;
+        targetNode.next = newNode;
+
+        /**
+         * Check if newNode was inserted at the end
+         * of the list and mark it as the tail if so.
+         */
+        if (newNode.next === null) {
+            this.tail = newNode;
+        }
+    }
+}
+
+/**
+ * Creates a new ListNode with the data value, and
+ * inserts it before the node with the target value.
+ * If the target doesn't exist, the new node is not
+ * inserted in the list.
+ * @param {*} data
+ * @param {*} target
+ */
+List.prototype.insertBefore = function(data, target) {
+    let prevNode = null;
+    let currentNode = this.head;
+    let inserted = false;
+
+    while (!inserted) {
+        if (currentNode.data === target) {
+            const newNode = new ListNode(data);
+
+            // Special case if target is head.
+            if (prevNode === null) {
+                this.head = newNode;
+                newNode.next = currentNode;
+                inserted = true;
+            } else {
+                prevNode.next = newNode;
+                newNode.next = currentNode;
+                inserted = true;
+            }
+        } else {
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+        }
+    }
+}
+
 const list = new List();
 
 list.addFirst(0);
@@ -69,6 +129,15 @@ list.addFirst(1);
 
 list.addLast(2);
 // Expected: 1 => 0 => 2 => null
+
+list.insertAfter(3, 1);
+// Expected: 1 => 3 => 0 => 2 => null
+
+list.insertBefore(4, 2);
+// Expected: 1 => 3 => 0 => 4 => 2 => null
+
+list.insertBefore(5, 1);
+// Expected: 5 => 1 => 3 => 0 => 4 => 2 => null
 
 const n1 = list.find(1);
 // Expected: A node with data 1 and pointing to node with data 0.
