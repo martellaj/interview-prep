fdaa9d9bb667c6b08a5708bc08b4401eb3fbb73a
Joe Martella <martellaj@live.com>
2017-11-17 16:14:41 -0800

    Converts linkedList to TS

--- src/dataStructures/linkedList/linkedList.ts
+++ src/dataStructures/linkedList/linkedList.ts
@@ -1,122 +1,132 @@
-function ListNode(data) {
-    this.data = data;
-    this.next = null;
-}
+class ListNode {
+    public data: number;
+    public next: ListNode;
 
-function List() {
-    this.head = null;
-    this.tail = null;
-}
-
-/**
- * Creates a ListNode with the given data value
- * and adds it to the beginning of the List.
- * @param {*} data
- */
-List.prototype.addFirst = function(data) {
-    const newNode = new ListNode(data);
-    newNode.next = this.head;
-    this.head = newNode;
-
-    if (!this.tail) {
-        this.tail = this.head;
+    constructor(data: number) {
+        this.data = data;
+        this.next = null;
     }
 }
 
-/**
- * Creates a ListNode with the given data value
- * and adds it to the end of the list.
- * @param {*} data
- */
-List.prototype.addLast = function(data) {
-    const newNode = new ListNode(data);
+class List {
+    public head: ListNode;
+    public tail: ListNode;
 
-    if (this.tail) {
-        this.tail.next = newNode;
-        this.tail = newNode;
-    } else {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    /**
+     * Creates a ListNode with the given data value
+     * and adds it to the beginning of the List.
+     * @param {*} data
+     */
+    public addFirst(data: number) {
+        const newNode = new ListNode(data);
+        newNode.next = this.head;
         this.head = newNode;
-        this.tail = newNode;
-    }
-}
 
-/**
- * Finds the first instance of a ListNode with the
- * given data value and returns it. Returns null
- * if no ListNode with the data value exists in the
- * list.
- * @param {*} data
- */
-List.prototype.find = function(data) {
-    let currentNode = this.head;
-
-    while (currentNode.next) {
-        if (currentNode.data === data) {
-            return currentNode;
-        } else {
-            currentNode = currentNode.next;
+        if (!this.tail) {
+            this.tail = this.head;
         }
     }
 
-    return null;
-}
-
-/**
- * Creates a new ListNode with the data value, and
- * inserts it after the node with the target value.
- * If the target doesn't exist, the new node is not
- * inserted in the list.
- * @param {*} data
- * @param {*} target
- */
-List.prototype.insertAfter = function(data, target) {
-    const targetNode = this.find(target);
-
-    if (targetNode) {
+    /**
+     * Creates a ListNode with the given data value
+     * and adds it to the end of the list.
+     * @param {*} data
+     */
+    public addLast(data: number) {
         const newNode = new ListNode(data);
-        newNode.next = targetNode.next;
-        targetNode.next = newNode;
 
-        /**
-         * Check if newNode was inserted at the end
-         * of the list and mark it as the tail if so.
-         */
-        if (newNode.next === null) {
+        if (this.tail) {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        } else {
+            this.head = newNode;
             this.tail = newNode;
         }
     }
-}
 
-/**
- * Creates a new ListNode with the data value, and
- * inserts it before the node with the target value.
- * If the target doesn't exist, the new node is not
- * inserted in the list.
- * @param {*} data
- * @param {*} target
- */
-List.prototype.insertBefore = function(data, target) {
-    let prevNode = null;
-    let currentNode = this.head;
-    let inserted = false;
+    /**
+     * Finds the first instance of a ListNode with the
+     * given data value and returns it. Returns null
+     * if no ListNode with the data value exists in the
+     * list.
+     * @param {*} data
+     */
+    public find(data: number) {
+        let currentNode = this.head;
 
-    while (!inserted) {
-        if (currentNode.data === target) {
-            const newNode = new ListNode(data);
-
-            // Special case if target is head.
-            if (prevNode === null) {
-                this.head = newNode;
-                newNode.next = currentNode;
-                inserted = true;
+        while (currentNode.next) {
+            if (currentNode.data === data) {
+                return currentNode;
             } else {
-                prevNode.next = newNode;
-                newNode.next = currentNode;
-                inserted = true;
+                currentNode = currentNode.next;
             }
-        } else {
-            prevNode = currentNode;
-            currentNode = currentNode.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * Creates a new ListNode with the data value, and
+     * inserts it after the node with the target value.
+     * If the target doesn't exist, the new node is not
+     * inserted in the list.
+     * @param {*} data
+     * @param {*} target
+     */
+    public insertAfter(data: number, target: number) {
+        const targetNode = this.find(target);
+
+        if (targetNode) {
+            const newNode = new ListNode(data);
+            newNode.next = targetNode.next;
+            targetNode.next = newNode;
+
+            /**
+             * Check if newNode was inserted at the end
+             * of the list and mark it as the tail if so.
+             */
+            if (newNode.next === null) {
+                this.tail = newNode;
+            }
+        }
+    }
+
+    /**
+     * Creates a new ListNode with the data value, and
+     * inserts it before the node with the target value.
+     * If the target doesn't exist, the new node is not
+     * inserted in the list.
+     * @param {*} data
+     * @param {*} target
+     */
+    public insertBefore(data: number, target: number) {
+        let prevNode: ListNode = null;
+        let currentNode = this.head;
+        let inserted = false;
+
+        while (!inserted) {
+            if (currentNode.data === target) {
+                const newNode = new ListNode(data);
+
+                // Special case if target is head.
+                if (prevNode === null) {
+                    this.head = newNode;
+                    newNode.next = currentNode;
+                    inserted = true;
+                } else {
+                    prevNode.next = newNode;
+                    newNode.next = currentNode;
+                    inserted = true;
+                }
+            } else {
+                prevNode = currentNode;
+                currentNode = currentNode.next;
+            }
         }
     }
 }
