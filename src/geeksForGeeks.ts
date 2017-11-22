caed7c22224e17f68cff7862ad5855609c24b35e
Joe Martella <martellaj@live.com>
2017-11-22 10:43:14 -0800

    Does findClosestPairSum problem

--- src/geeksForGeeks.ts
+++ src/geeksForGeeks.ts
@@ -12,4 +12,40 @@ function insertSortedLinkedList() {
     list.print();
 }
 
-insertSortedLinkedList();
+/**
+ * Finds the pair in array whose sum is closest to x.
+ * Assumes that the array is sorted.
+ * @param array
+ * @param x
+ */
+function findClosestPairSum(array: number[], x: number) {
+    if (!array || array.length < 2) {
+        return null;
+    } else if (array.length === 2 || x < (array[0] + array[1])) {
+        return [array[0], array[1]];
+    }
+
+    let prev = array[0] + array[1];
+    let curr = undefined;
+
+    for (let i = 0; i < array.length - 1; i++) {
+        curr = array[i] + array[i + 1];
+
+        if (x >= prev && x <= curr) {
+            const d1 = x - prev;
+            const d2 = curr - x;
+
+            if (d1 < d2 || d1 === 0) {
+                return [array[i - 1], array[i]];
+            } else {
+                return [array[i], array[i + 1]];
+            }
+        }
+    }
+
+    return [array[array.length - 2], array[array.length - 1]];
+}
+
+const array = [2, 5, 6, 10, 11];
+const closestPair = findClosestPairSum(array, 10);
+debugger;
